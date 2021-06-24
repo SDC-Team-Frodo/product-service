f5719c7783649acbddb6ed9ee2c95dbb9f246932
Joseph Balaoing <joseph.balaoing1@yahoo.com>
2021-06-24 12:05:46 -0700

    Implemented get products endpoint

--- server/server.js
+++ server/server.js
@@ -4,12 +4,23 @@ const Pool = require('pg').Pool;
 const dbconfig = require('./config.js').db;
 
 const pool = new Pool(dbconfig);
+pool.connect();
 
 const app = express();
 const port = 5000;
 
-app.get('/products', (res, req) => {
+app.get('/products', (req, res) => {
+  var searchParams = new URLSearchParams(req.url.replace('/products', ''));
+  var page = searchParams.get('page');
+  var count = searchParams.get('count');
 
+  var start = page * count - count + 1;
+  var end = page * count;
+  var sql = `SELECT * FROM products WHERE id >= ${start} AND id <= ${end}`;
+  pool.query(sql)
+    .then(response => {
+      res.end(JSON.stringify(response.rows));
+    });
 });
 
 app.get('/products/:product_id', (res, req) => {
