d671fd3845c343325fb0828495b0df10173afef9
Joseph Balaoing <joseph.balaoing1@yahoo.com>
2021-06-24 10:54:06 -0700

    Created server.js with end point skeletons

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get('/products', (res, req) => {
+
+});
+
+app.get('/products/:product_id', (res, req) => {
+
+});
+
+app.get('/products/:product_id/styles', (res, req) => {
+
+});
+
+app.get('/products/:product_id/related', (res, req) => {
+
+});
+
