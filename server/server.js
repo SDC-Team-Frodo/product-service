020bad6dea5d403ebc42a188077c961de5870ee9
Joseph Balaoing <joseph.balaoing1@yahoo.com>
2021-06-24 13:31:53 -0700

    Created query for /products/:product_id for products, working on feature query

--- server/server.js
+++ server/server.js
@@ -10,9 +10,13 @@ const app = express();
 const port = 5000;
 
 app.get('/products', (req, res) => {
-  var searchParams = new URLSearchParams(req.url.replace('/products', ''));
-  var page = searchParams.get('page');
-  var count = searchParams.get('count');
+  var page = req.query.page || 1;
+  var count = req.query.count || 5;
+  if (page < 1) {
+    res.status(400);
+    res.end();
+    return;
+  }
 
   var start = page * count - count + 1;
   var end = page * count;
@@ -20,11 +24,49 @@ app.get('/products', (req, res) => {
   pool.query(sql)
     .then(response => {
       res.end(JSON.stringify(response.rows));
+    })
+    .catch(err => {
+      res.status(500);
+      res.end();
     });
 });
 
-app.get('/products/:product_id', (res, req) => {
+app.get('/products/:product_id', (req, res) => {
+  console.log(req.params);
+  var queries = [];
+  var product = {};
 
+  var productQuery = `SELECT * FROM products WHERE id=${req.params['product_id']}`;
+  var featureQuery = `SELECT feature, value FROM features WHERE product_id=${req.params['product_id']}`;
+  queries.push(pool.query(productQuery)
+    .then(response => {
+      for (var key in response.rows[0]) {
+        product[key] = response.rows[0][key];
+      }
+    })
+    .catch(err => {
+      res.status(500);
+      res.end();
+      return;
+    })
+  );
+  queries.push(pool.query(featureQuery)
+    .then(response => {
+      console.log(response.rows);
+      // for (var key in response.rows[0]) {
+      //   product[key] = response.rows[0][key];
+      // }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500);
+      res.end();
+      return;
+    })
+  );
+
+  Promise.all(queries)
+    .then()
 });
 
 app.get('/products/:product_id/styles', (res, req) => {
